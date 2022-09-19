d77156f73e530ee51ffa4456a6e6fca474dcd96f
MindScape00 <MindScape00@users.noreply.github.com>
2022-09-18 17:32:02 -0700

    Allow filter scoping between ann & all & default

--- Announce_Ignore/Announce_Ignore.lua
+++ Announce_Ignore/Announce_Ignore.lua
@@ -1,13 +1,43 @@
+local ignore_scope = 2
+local ignore_scope_def = { "Only Default (Chat/Channel)", "Default+Announce & Phase Announce", "Default+All System Messages" }
+
 local function ChatFilter(Self,Event,Message)
-	local playerName = Message:match("|Hplayer:(.-)|h")
-	if playerName then
-		if C_FriendList.IsIgnored(playerName) then
-			print(playerName.." is Ignored. Announce should be hidden.")
-			return;
+	
+	-- dev message testing
+	--local clearmsg = gsub(Message,"|cff%x%x%x%x%x%x","");
+	--local clearmsg = clearmsg:gsub("|r","");
+	--local clearmsg = Message:gsub("|","\\")
+	--print(clearmsg)
+	--print(Message)
+	
+	if ignore_scope then
+		if ignore_scope == 1 then return; -- return if default, we do nothing..
+		elseif ignore_scope == 2 then -- If Filter Scope should be ann/phase ann only
+			local annCheck = Message:match("|cff00a2d7Epsilon|r.-:")
+			local phaseCheck = Message:match("|cff00a2d7%[.-%]|r |cff.-|Hplayer:.-:")
+			
+			if annCheck or phaseCheck then
+				local playerName = Message:match("|Hplayer:(.-)|h")
+				--print(playerName)
+				if playerName then
+					if C_FriendList.IsIgnored(playerName) then
+						--print(playerName.." is Ignored. Message should be hidden.")
+						return true;
+					end
+				end
+			end
+		elseif ignore_scope == 3 then -- If All Sys Messages
+			local playerName = Message:match("|Hplayer:(.-)|h")
+			--print(playerName)
+			if playerName then
+				if C_FriendList.IsIgnored(playerName) then
+					--print(playerName.." is Ignored. Message should be hidden.")
+					return true;
+				end
+			end
 		end
 	end
 end
-
 
 ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", ChatFilter)
 ChatFrame_AddMessageEventFilter("CHAT_MSG_ACHIEVEMENT", ChatFilter);
@@ -24,3 +54,12 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_BG_SYSTEM_ALLIANCE", ChatFilter);
 ChatFrame_AddMessageEventFilter("CHAT_MSG_BG_SYSTEM_NEUTRAL", ChatFilter);
 ChatFrame_AddMessageEventFilter("CHAT_MSG_TARGETICONS", ChatFilter);
 ChatFrame_AddMessageEventFilter("CHAT_MSG_BN_CONVERSATION_NOTICE", ChatFilter);
+
+SLASH_ANNOUNCE_IGNORE1 = '/annignore';
+function SlashCmdList.ANNOUNCE_IGNORE(msg, editbox) -- 4.
+	ignore_scope = ignore_scope + 1
+	if ignore_scope > 3 then
+		ignore_scope = 1
+	end
+	print("Announce_Ignore: Filter set to "..ignore_scope_def[ignore_scope].." messages.")
+end
